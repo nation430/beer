561b224af29a7b160db528b7b0c58adb7f5270e2
Tonye Nation <tonyenation430@gmail.com>
2020-09-10 22:12:51 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+  document.querySelector(".drink").src = anything;
+}
+function changeBgColor(color) {
+  let section1 = document.querySelector(".section1");
+  section1.style.background = color;
+}
